bfcc8bcce4896697d71fd5bcbbb4bd0781abbe0c
Davi <davisilva@unochapeco.edu.br>
2025-02-24 19:01:14 -0300

    updated ci container

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -6,7 +6,7 @@ import Landing from "../components/Landing";
 import Pricing from "../components/Pricing";
 
 export default () => {
-  console.log("123", process.env.PRODUCTION_DOMAIN);
+  console.log("1234", process.env.PRODUCTION_DOMAIN);
   return (
     <div>
       <StylesProvider>
