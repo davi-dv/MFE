c038388bf524bd5691a313c6cabced3c272d0248
Davi <davisilva@unochapeco.edu.br>
2025-02-24 18:45:31 -0300

    updated ci container

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -6,7 +6,7 @@ import Landing from "../components/Landing";
 import Pricing from "../components/Pricing";
 
 export default () => {
-  console.log("12");
+  console.log("123");
   return (
     <div>
       <StylesProvider>
