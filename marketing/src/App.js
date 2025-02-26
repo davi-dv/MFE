19fdeaff0a267a3ce825e27bec5de53fd0e6bce8
Davi <davisilva@unochapeco.edu.br>
2025-02-26 19:59:40 -0300

    fix: testing full pipeline mkt

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -6,7 +6,7 @@ import Landing from "../components/Landing";
 import Pricing from "../components/Pricing";
 
 export default () => {
-  console.log("teste mkt23344");
+  console.log("teste mkt233444");
   return (
     <div>
       <StylesProvider>
