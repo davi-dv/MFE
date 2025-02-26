7c9a141f9a8e5456ba357b5d566e46f3d7366fee
Davi <davisilva@unochapeco.edu.br>
2025-02-25 21:57:17 -0300

    fix: testing full pipeline

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("containnerrr");
+  console.log("containnerrr1");
   return (
     <div>
       <h1>Hi there</h1>
