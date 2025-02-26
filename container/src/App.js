718b34201665a631fd8aa6e8e28a20ad9cfa717e
Davi <davisilva@unochapeco.edu.br>
2025-02-26 20:17:58 -0300

    fix: testing full pipeline mkt

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("containnerrr1232");
+  console.log("containnerrr12325");
   return (
     <div>
       <h1>Hi there</h1>
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -6,7 +6,7 @@ import Landing from "../components/Landing";
 import Pricing from "../components/Pricing";
 
 export default () => {
-  console.log("teste mkt2334444");
+  console.log("teste mkt23344445");
   return (
     <div>
       <StylesProvider>
