fa7b017e740d8eafd4e3a8ed22678e7dadc6d8c1
Davi <davisilva@unochapeco.edu.br>
2025-03-02 11:10:08 -0300

    feat: added scope in css library to not have colission em prod

--- container/src/App.js
+++ container/src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
 
+const generatedClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default () => {
   console.log("cc");
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generatedClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
